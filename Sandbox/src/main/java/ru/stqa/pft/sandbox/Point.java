6a97de6df6d9821f0679b45af26372bb91e0f065
dmitry-alexandrov <dmitry.alexandrov.u@gmail.com>
2017-03-16 12:40:01 +0300

    Домашнее задание №1 (4)

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,4 +9,17 @@ public class Point {
 
   public double y;
 
+    public Point(double x, double y) {
+
+      this.x = x;
+
+      this.y = y;
+
+    }
+
+  public double distance(Point a) {
+
+    return Math.sqrt(Math.pow((this.x - a.x), 2) + Math.pow((this.y - a.y), 2));
+
+  }
 }
